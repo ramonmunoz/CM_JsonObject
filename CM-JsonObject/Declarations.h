15f403fdf7c3394ec425074e946cc9ca13990568
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-07-18 09:52:45 -0500

    Weather Object model added

--- CM-JsonObject/Declarations.h
+++ CM-JsonObject/Declarations.h
@@ -6,8 +6,16 @@
 //  Copyright (c) 2015 Smartplace. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "SBJson.h"
+
+//Debug
+#define nDebugEnable        1
+#define print(x)            if(nDebugEnable){(x);}
+
+//Json modes
+#define nGET                0
+#define nPOST               1
 
 @interface Declarations : NSObject
-
++ (NSDictionary *)getWeather:(float)lat and:(float)lng;
 @end
